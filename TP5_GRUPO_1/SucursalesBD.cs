2a1a8ed090fed390e27e0106c43499762f85ecf2
jorgevmanci@gmail.com <Andre*001>
2024-05-04 17:05:55 -0300

    Agregar archivos de proyecto.

---
+++ TP5_GRUPO_1/SucursalesBD.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TP5_GRUPO_1
+{
+    public class SucursalesBD
+    {
+
+        const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=BDSucursales;Integrated Security=True";
+
+        private DataSet EjecutarConulta(string consulta, string nombreTabla)
+        {
+            SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(consulta, sqlConnection);
+            DataSet resultados = new DataSet();
+            sqlDataAdapter.Fill(resultados, nombreTabla);
+
+            sqlConnection.Close();
+
+            return resultados;
+        }
+
+        private int EjecutarABM(string sentenciaABM)
+        {
+            int resultado = 0;
+
+            SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand(sentenciaABM, sqlConnection);
+            resultado = sqlCommand.ExecuteNonQuery();
+
+            return resultado;
+        }
+
+
+    }
+}
