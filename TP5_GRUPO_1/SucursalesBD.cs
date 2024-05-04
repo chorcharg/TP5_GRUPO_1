d5d17400233787b7c728c71e038a986e4f0e643d
jorgevmanci@gmail.com <Andre*001>
2024-05-04 17:29:34 -0300

    Creacion de formularios (vacios) y de una clase para gesrtionar en la base de datos

--- TP5_GRUPO_1/SucursalesBD.cs
+++ TP5_GRUPO_1/SucursalesBD.cs
@@ -12,6 +12,32 @@ namespace TP5_GRUPO_1
 
         const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=BDSucursales;Integrated Security=True";
 
+        //EJEMPLO DE USO EjecutarABM, INCOMPLETO (hay que construir la query
+        private int agregarSucursal(string nombre, string descripcion, int idProvicia, string direccion)
+        {
+            string query = "";
+
+
+            return EjecutarABM(query); //Esta devuelve un INT, por eso se puede usar en el return evitando crear una variable INT
+        }
+
+
+        //EJEMPLO DE USO EjecutarConsulta
+        private DataSet ListarTodos()
+        {
+            string query = "SELECT " +
+                                "SUC.Id_Sucursal, " +
+                                "SUC.NombreSucursal as Nombre, " +
+                                "SUC.DescripcionSucursal as Descripcion, " +
+                                "PROV.DescripcionProvincia, " +
+                                "SUC.DireccionSucursal as Direccion " +
+                          "FROM sucursal as SUC "+
+                          "JOIN Provincia as PROV on SUC.Id_ProvinciaSucursal = PROV.Id_Provincia"; //delegamos en la DB traernos el nombre de la provincia (visto en laboratorio)
+
+            return EjecutarConulta(query, "Sucursales");
+        }
+
+        //para querys SELECT
         private DataSet EjecutarConulta(string consulta, string nombreTabla)
         {
             SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
@@ -26,6 +52,8 @@ namespace TP5_GRUPO_1
             return resultados;
         }
 
+
+        //para queyys de DELETE, UPDTE E INSERT
         private int EjecutarABM(string sentenciaABM)
         {
             int resultado = 0;
