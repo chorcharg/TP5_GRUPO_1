41c54d8439a901eef65ba9b146c4eeffd94deaf0
RoOjeda <rpojeda34@gmail.com>
2024-05-06 03:34:58 -0300

    Formulario, llenar base de datos
    Volví al cambio de Jorge, porque hice algo mal y no pude solucionar, por eso aparece como un commit, lo refresque solamente y luego agregue el formulario y una parte de agregar datos a la Base

---
+++ TP5_GRUPO_1/AgregarSucursal.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TP5_GRUPO_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private const string CadenaConexion = @"Data Source=localhost\SQLEXPRESS; Initial Catalog=BDSucursales; Integrated Security=True";
+        private  string consultaSql;
+        private int FilasActualizadas;
+        private Conexion conexion = new Conexion();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void LbAgregarSuc_Click(object sender, EventArgs e)
+        {
+            // Establecer la consulta SQL (asigna un valor a consultaSql)
+            consultaSql = "INSERT INTO Sucursal (NombreSucursal, DescripcionSucursal, Id_ProvinciaSucursal, DireccionSucursal) VALUES (@Nombre, @Descripcion, @IDProvincia, @Direccion)";
+            // Crear el objeto SqlCommand
+            SqlCommand sqlCommand = new SqlCommand(consultaSql);
+
+            // Agregar los parámetros
+            sqlCommand.Parameters.AddWithValue("@Nombre", TxtNombreSuc.Text);
+            sqlCommand.Parameters.AddWithValue("@Descripcion", TxtDescripcion.Text);
+            sqlCommand.Parameters.AddWithValue("@IDProvincia", DdlProvincia.SelectedValue);
+            sqlCommand.Parameters.AddWithValue("@Direccion", TxtDireccion.Text);
+
+            // Ejecutar la consulta
+            FilasActualizadas = conexion.EjecutarConexion(sqlCommand);
+        }
+    }    
+ }
