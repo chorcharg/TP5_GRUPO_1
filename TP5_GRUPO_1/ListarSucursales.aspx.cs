dcf942a7ec569f19ae1706f8cbaad8db4f8b1cde
Dani5750 <gdanielm57@gmail.com>
2024-05-08 19:15:07 -0300

    Agregue codigo para cargar el gridview que lista las sucursales

--- TP5_GRUPO_1/ListarSucursales.aspx.cs
+++ TP5_GRUPO_1/ListarSucursales.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,9 +11,20 @@ namespace TP5_GRUPO_1
     
     public partial class WebForm2 : System.Web.UI.Page
     {
+        SucursalesBD sucursalesBD = new SucursalesBD();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                CargarSucursales();
+            }
+        }
+        private void CargarSucursales()
+        {
+            DataSet sucursalesDataSet = sucursalesBD.ListarSucursales("sucursales");
+            GvListado.DataSource = sucursalesDataSet;
+            GvListado.DataBind();
         }
     }
 }
